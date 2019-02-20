ef80e176bb69c5106513d389f2ded98fa99db117
jacobi184 <47827230+jacobi184@users.noreply.github.com>
2019-02-20 11:13:43 -0600

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	
+	var userfeed = new Instafeed({
+		get: 'user',
+		userId: '9268876548',
+		limit :12,
+		resolution: 'standard_resolution',
+		accessToken: '9268876548.1677ed0.ebd323f1f5254d2ca78d92490736ed84',
+		sortBy: 'most-recent',
+		template: '<div class="gallary"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid" /></a></div>',
+	});
+	userFeed.run();
+});
